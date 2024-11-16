c1ab9595e1af72cb84ec9b2e8e94fd323c3df2cf
tahreemimam23 <imamtahreem23@gmail.com>
2024-11-16 11:16:51 +0530

    user feature updated

--- src/app/user/my-sports/my-sports.component.ts
+++ src/app/user/my-sports/my-sports.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../service/user.service';
+import { map, Observable } from 'rxjs';
+import { UserData } from '../../model/user-data.model';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'cx-my-sports',
   templateUrl: './my-sports.component.html',
   styleUrl: './my-sports.component.css'
 })
-export class MySportsComponent {
+export class MySportsComponent implements OnInit {
+  sports$: Observable<any>;
+  userDetails: UserData;
+
+  constructor(private userService: UserService, private authService: AuthService) { }
+  
+  ngOnInit(): void {
+    this.userDetails = this.authService.userDetails;
+    this.sports$ = this.userService.getAllSports().pipe(
+      map(sports=>{
+        return sports.filter(sport => sport.email === this.userDetails.email)
+      })
+    );
+  }
 
 }
